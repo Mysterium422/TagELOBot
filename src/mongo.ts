6de6d6fd27e70d9a31bc9c1caa5f7d400e5e52e6
Mysterium <mysterium422@gmail.com>
2022-08-30 13:29:26 -0400

    Fixed mongo framework

--- src/mongo.ts
+++ src/mongo.ts
@@ -1,5 +1,5 @@
-import config from "./config"
-import { consolelog } from "./utils"
+import config from "../config"
+import { consolelog } from "../utils"
 
 const mongoose = require("mongoose")
 
@@ -16,7 +16,45 @@ mongoose
 		console.error(err)
 	})
 
-const profileSchema = new mongoose.Schema({
+export enum MODELS {
+	Users = "users"
+}
+
+type UserRecord =
+	| {
+			reason: "game"
+			opponent: string
+			elo: number
+			time: number
+	  }
+	| {
+			reason: "duel"
+			opponent: string
+			elo: number
+			time: number
+	  }
+	| {
+			reason: "admin"
+			elo: number
+			time: number
+	  }
+
+export type MongoUser = {
+	userID: string
+	elo: number
+	username: string
+	uuid: string
+	wins: number
+	losses: number
+	records: UserRecord[]
+	deviation: number
+}
+
+interface MongoMap {
+	[MODELS.Users]: MongoUser
+}
+
+const userSchema = new mongoose.Schema({
 	userID: { type: String, require: true, unique: true },
 	elo: { type: Number, require: true, default: 1000 },
 	username: { type: String, require: true },
@@ -27,8 +65,42 @@ const profileSchema = new mongoose.Schema({
 	deviation: { type: Number, require: true, default: 100 }
 })
 
-const profileModel = mongoose.model("user", profileSchema)
+const usersModel = mongoose.model("user", userSchema)
+const models = {
+	users: usersModel
+}
 
-module.exports = {
-	profile: profileModel
+export const findOne = <K extends keyof MongoMap>(
+	model: K,
+	query: Partial<MongoMap[K]>
+): Promise<MongoMap[K]> => {
+	return models[model].findOne(query).toJSON()
+}
+
+export const findOneAndReplace = <K extends keyof MongoMap>(
+	model: K,
+	query: Partial<MongoMap[K]>,
+	newvalue: MongoMap[K]
+) => {
+	return models[model].findOneAndReplace(query, newvalue)
+}
+
+export const find = <K extends keyof MongoMap>(
+	model: K,
+	query: Partial<MongoMap[K]>
+): Promise<MongoMap[K][]> => {
+	return models[model].find(query)
+}
+
+export const findOneAndDelete = <K extends keyof MongoMap>(
+	model: K,
+	query: Partial<MongoMap[K]>
+) => {
+	return models[model].findOneAndDelete(query)
+}
+
+export const create = async <K extends keyof MongoMap>(model: K, data: MongoMap[K]) => {
+	let user = await models[model].create(data)
+	user.save()
+	return
 }
