90a0420ddce7088d1dd1d492f9beaf5109c707dc
Mysterium <mysterium422@gmail.com>
2022-08-30 23:09:53 -0400

    Adds leave command

--- src/commands/base.ts
+++ src/commands/base.ts
@@ -2,6 +2,10 @@ import { CommandParameters } from "../CommandParameters"
 import Discord from "discord.js"
 import { hasStaffPermission, Staff } from "../utils"
 import config from "../config"
+import * as queue from "../handlers/queue"
+import * as games from "../handlers/game"
+import * as db from "../db"
+import * as mongo from "../mongo"
 
 export default {
 	run: async ({ message }: CommandParameters) => {
---
+++ src/commands/leave.ts
@@ -0,0 +1,114 @@
+import { CommandParameters } from "../CommandParameters"
+import Discord from "discord.js"
+import { addAudit, hasStaffPermission, simulateDM, Staff } from "../utils"
+import config from "../config"
+import * as queue from "../handlers/queue"
+import * as games from "../handlers/game"
+
+export default {
+	run: async ({ message, client }: CommandParameters) => {
+		if (
+			message.channel.id != config.mainChannelID &&
+			message.channel.id != config.queueChannelID &&
+			message.channel.id != config.commandsChannelID
+		) {
+			return
+		}
+
+		if (message.channel.id != config.queueChannelID) {
+			setTimeout(async function () {
+				message.delete()
+			}, 200)
+			return message.author
+				.send({
+					embeds: [
+						new Discord.MessageEmbed()
+							.setColor("NOT_QUITE_BLACK")
+							.setDescription(`That command goes in <#${config.queueChannelID}>`)
+					]
+				})
+				.catch((err) =>
+					simulateDM(
+						message,
+						new Discord.MessageEmbed()
+							.setColor("NOT_QUITE_BLACK")
+							.setDescription(`That command goes in <#${config.queueChannelID}>`),
+						client
+					)
+				)
+		}
+		addAudit(`${message.author.id} tried to leave a game`)
+		setTimeout(async function () {
+			message.delete()
+		}, 200)
+		if (games.inGame(message.author.id)) {
+			return message.channel.send({
+				embeds: [
+					new Discord.MessageEmbed()
+						.setColor("NOT_QUITE_BLACK")
+						.setDescription(
+							`You are in a game against <@!${games.findOpponent(
+								message.author.id
+							)}>. If you can't make it you must either forfeit (do ${
+								config.prefix
+							}forfeit <@!${games.findOpponent(message.author.id)}>) or abort (do ${
+								config.prefix
+							}abort)`
+						)
+				]
+			})
+		}
+
+		if (!queue.inQueue(message.author.id)) {
+			return message.author
+				.send({
+					embeds: [
+						new Discord.MessageEmbed()
+							.setColor("NOT_QUITE_BLACK")
+							.setDescription("You are not in a queue")
+					]
+				})
+				.catch((err) =>
+					simulateDM(
+						message,
+						new Discord.MessageEmbed()
+							.setColor("NOT_QUITE_BLACK")
+							.setDescription("You are not in a queue"),
+						client
+					)
+				)
+		}
+
+		queue.leave(message.author.id)
+		message.channel.send({
+			embeds: [
+				new Discord.MessageEmbed()
+					.setColor("BLUE")
+					.setDescription(
+						`There ${queue.queueSize() != 1 ? "are" : "is"} ${queue.queueSize()} ${
+							queue.queueSize() != 1 ? "players" : "player"
+						} in the queue`
+					)
+			]
+		})
+		message.author
+			.send({
+				embeds: [
+					new Discord.MessageEmbed()
+						.setColor("RED")
+						.setDescription("Removed you from the queue")
+				]
+			})
+			.catch((err) =>
+				simulateDM(
+					message,
+					new Discord.MessageEmbed()
+						.setColor("RED")
+						.setDescription("Removed you from the queue"),
+					client
+				)
+			)
+		addAudit(`${message.author.id} left a game`)
+		return
+	}
+}
