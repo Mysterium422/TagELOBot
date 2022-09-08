fdae424c5604f669f40507db89f9314684eb34c5
Mysterium <mysterium422@gmail.com>
2022-09-08 14:26:13 -0400

    2.0.1 - Keeps scan msg left aligned

--- src/buttons/scan.ts
+++ src/buttons/scan.ts
@@ -60,42 +60,6 @@ export default {
 
 		addAudit(`${button.member.id} scanned ${accused}`)
 
-		// let role1 = await button.guild.roles.cache.get(config.rankedRoleID)
-		// let role2 = await message.guild.roles.cache.get(config.scanRoleID)
-		// if (!role1) throw new Error("Couldnt find ranked role")
-		// if (!role2) throw new Error("Couldnt find scan role")
-		// message.member.roles.add(role2)
-		// message.member.roles.remove(role1)
-		// accused.roles.add(role2)
-		// accused.roles.remove(role1)
-
-		// let channel = await message.guild.channels.create("scan-request", {
-		// 	type: "GUILD_TEXT",
-		// 	parent: config.rankedCategoryID,
-		// 	permissionOverwrites: [
-		// 		{
-		// 			id: config.guildID,
-		// 			deny: ["VIEW_CHANNEL"]
-		// 		},
-		// 		{
-		// 			id: config.staffRoleID,
-		// 			allow: ["VIEW_CHANNEL"]
-		// 		},
-		// 		{
-		// 			id: config.scannerRoleID,
-		// 			allow: ["VIEW_CHANNEL"]
-		// 		},
-		// 		{
-		// 			id: message.author.id,
-		// 			allow: ["VIEW_CHANNEL"]
-		// 		},
-		// 		{
-		// 			id: accused.id,
-		// 			allow: ["VIEW_CHANNEL"]
-		// 		}
-		// 	]
-		// })
-
 		await button.channel.permissionOverwrites.create(config.scanRoleID, {
 			VIEW_CHANNEL: true
 		})
@@ -116,10 +80,10 @@ export default {
 
 		await thread.send({
 			content: `**<@!${button.member.id}> has requested a scan for <@!${accused}>!**
-		<@!${accused}> please do not log off Hypixel. If you do so, you'll receive a **Red Card**.
-		<@!${button.member.id}> please stay online to ensure <@!${accused}> does not log off Hypixel. If they do, send a screenshot of it here.
-		Neither of you should queue until directed by staff.
-		If a <@&${config.scannerRoleID}> hasn't responded within 15 minutes, you may both leave the server and freely queue`
+<@!${accused}> please do not log off Hypixel. If you do so, you'll receive a **Red Card**.
+<@!${button.member.id}> please stay online to ensure <@!${accused}> does not log off Hypixel. If they do, send a screenshot of it here.
+Neither of you should queue until directed by staff.
+If a <@&${config.scannerRoleID}> hasn't responded within 15 minutes, you may both leave the server and freely queue`
 		})
 
 		return
